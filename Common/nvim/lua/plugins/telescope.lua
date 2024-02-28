786e8a499c139e824a382c728152adb3ff74919a
Dev Vrat Singh <itsdvvs.18@gmail.com>
2024-02-28 16:06:23 +0530

    shortcut to edit nvim configs directly

--- Common/nvim/lua/plugins/telescope.lua
+++ Common/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,9 @@
+function edit_neovim_conf()
+  require('telescope.builtin').find_files {
+    cwd = "~/.config/nvim"
+  }
+end
+
 return {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.5',
@@ -9,6 +15,7 @@ return {
 		shortcut.set('n', '<leader>ff', builtin.find_files, {})
 		shortcut.set('n', '<leader>fg', builtin.live_grep, {})
 		shortcut.set('n', '<leader>fb', builtin.buffers, {})
+		shortcut.set('n', '<leader>fc', ":lua edit_neovim_conf()<CR>", {})
 	end
 }
 
